fe4d118887eccf4ba43559d853252eff1c00f2dc
Björn Albers <bjoernalbers@gmail.com>
2014-11-09 10:50:12 +0100

    Upgrade to RSpec 3.1+
    - use new RSpec syntax and fix deprecation warnings
    - lock RSpec version

--- nagiosplugin.gemspec
+++ nagiosplugin.gemspec
@@ -7,7 +7,7 @@ Gem::Specification.new do |spec|
   spec.name        = 'nagiosplugin'
   spec.version     = NagiosPlugin::VERSION
   spec.authors     = ['Björn Albers']
-  spec.email       = ['bjoernalbers@googlemail.com']
+  spec.email       = ['bjoernalbers@gmail.com']
   spec.homepage    = 'https://github.com/bjoernalbers/nagiosplugin'
   spec.summary     = "#{spec.name}-#{spec.version}"
   spec.description = 'The one Nagios Plugin framework, forged in the fires of Mount Doom.'
@@ -22,6 +22,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'cucumber'
   spec.add_development_dependency 'aruba'
   spec.add_development_dependency 'guard-cucumber'
-  spec.add_development_dependency 'rspec'
+  spec.add_development_dependency 'rspec', '~> 3.1'
   spec.add_development_dependency 'guard-rspec'
 end
--- spec/nagiosplugin/plugin_spec.rb
+++ spec/nagiosplugin/plugin_spec.rb
@@ -6,31 +6,33 @@ module NagiosPlugin
 
     describe '.check' do
       before do
-        plugin.stub(:nagios_plugin_exit_code  => nil,
-                    :nagios_plugin_output     => nil)
-        Plugin.stub(:puts                     => nil,
-                    :exit                     => nil,
-                    :new                      => plugin)
+        allow(plugin).to receive_messages(
+          nagios_plugin_exit_code:  nil,
+          nagios_plugin_output:     nil)
+        allow(Plugin).to receive_messages(
+          puts: nil,
+          exit: nil,
+          new: plugin)
       end
 
       it 'displays the plugin output on stdout' do
-        plugin.should_receive(:nagios_plugin_output).
+        allow(plugin).to receive(:nagios_plugin_output).
           and_return('chunky bacon')
-        Plugin.should_receive(:puts).with('chunky bacon')
         Plugin.check
+        expect(Plugin).to have_received(:puts).with('chunky bacon')
       end
 
       it 'exits with the status exit code' do
-        plugin.should_receive(:nagios_plugin_exit_code).and_return(42)
-        Plugin.should_receive(:exit).with(42)
+        allow(plugin).to receive(:nagios_plugin_exit_code).and_return(42)
         Plugin.check
+        expect(Plugin).to have_received(:exit).with(42)
       end
 
       context 'when an exception was raised' do
-        let(:exception) { StandardError.new }
+        let(:exception) { StandardError.new('Oops!') }
 
         before do
-          Plugin.stub(:new).and_return { raise 'Oops!' }
+          allow(Plugin).to receive(:new).and_raise(exception)
         end
 
         it 'rescues the exception' do
@@ -38,16 +40,16 @@ module NagiosPlugin
         end
 
         it 'exits with exit code unknown' do
-          Plugin.should_receive(:exit).with(3)
           Plugin.check
+          expect(Plugin).to have_received(:exit).with(3)
         end
 
         it 'displays the exception and backtrace on stdout' do
-          StandardError.any_instance.stub(:backtrace).
-            and_return(%w(Chunky Bacon))
-          Plugin.should_receive(:puts).
-            with("PLUGIN UNKNOWN: Oops!\n\nChunky\nBacon")
+          allow(StandardError).to receive(:new).and_return(exception)
+          allow(exception).to receive(:backtrace).and_return(%w(Chunky Bacon))
           Plugin.check
+          expect(Plugin).to have_received(:puts).
+            with("PLUGIN UNKNOWN: Oops!\n\nChunky\nBacon")
         end
       end
     end
@@ -68,28 +70,40 @@ module NagiosPlugin
 
     describe '#nagios_plugin_status' do
       it 'returns unknown when not critical, warning or ok' do
-        plugin.stub(:critical? => false, :warning? => false, :ok? => false)
+        allow(plugin).to receive_messages(
+          critical?: false,
+          warning?:  false,
+          ok?:       false)
         expect(plugin.send(:nagios_plugin_status)).to eq(:unknown)
       end
 
       it 'returns critical when critical' do
-        plugin.stub(:critical? => true, :warning? => true, :ok? => true)
+        allow(plugin).to receive_messages(
+          critical?: true,
+          warning?:  true,
+          ok?:       true)
         expect(plugin.send(:nagios_plugin_status)).to eq(:critical)
       end
 
       it 'returns warning when warning but not critical' do 
-        plugin.stub(:critical? => false, :warning? => true, :ok? => true)
+        allow(plugin).to receive_messages(
+          critical?: false,
+          warning?:  true,
+          ok?:       true)
         expect(plugin.send(:nagios_plugin_status)).to eq(:warning)
       end
 
       it 'returns ok when ok but not critical or warning' do
-        plugin.stub(:critical? => false, :warning? => false, :ok? => true)
+        allow(plugin).to receive_messages(
+          critical?: false,
+          warning?:  false,
+          ok?:       true)
         expect(plugin.send(:nagios_plugin_status)).to eq(:ok)
       end
 
-      it 'saves the status' do
+      it 'caches the status' do
         [:critical?, :warning?, :ok?].each do |check|
-          plugin.should_receive(check).once.and_return(false)
+          allow(plugin).to receive(check).once.and_return(false)
         end
         expect(plugin.send(:nagios_plugin_status)).to eq(:unknown)
         expect(plugin.send(:nagios_plugin_status)).to eq(:unknown)
@@ -98,44 +112,44 @@ module NagiosPlugin
 
     describe '#nagios_plugin_exit_code' do
       it 'returns 3 when unknown' do
-        plugin.should_receive(:nagios_plugin_status).and_return(:unknown)
+        allow(plugin).to receive(:nagios_plugin_status).and_return(:unknown)
         expect(plugin.send(:nagios_plugin_exit_code)).to eq(3)
       end
 
       it 'returns 2 when critical' do
-        plugin.should_receive(:nagios_plugin_status).and_return(:critical)
+        allow(plugin).to receive(:nagios_plugin_status).and_return(:critical)
         expect(plugin.send(:nagios_plugin_exit_code)).to eq(2)
       end
 
       it 'returns 1 when warning' do
-        plugin.should_receive(:nagios_plugin_status).and_return(:warning)
+        allow(plugin).to receive(:nagios_plugin_status).and_return(:warning)
         expect(plugin.send(:nagios_plugin_exit_code)).to eq(1)
       end
       
       it 'returns 0 when ok' do
-        plugin.should_receive(:nagios_plugin_status).and_return(:ok)
+        allow(plugin).to receive(:nagios_plugin_status).and_return(:ok)
         expect(plugin.send(:nagios_plugin_exit_code)).to eq(0)
       end
     end
 
     describe '#nagios_plugin_output' do
       before do
-        plugin.stub(:nagios_plugin_status)
+        allow(plugin).to receive(:nagios_plugin_status)
       end
 
       it 'joins the service name and the upcased status' do
-        plugin.should_receive(:nagios_plugin_service).and_return('FRIED')
-        plugin.should_receive(:nagios_plugin_status).and_return(:chicken)
+        allow(plugin).to receive(:nagios_plugin_service).and_return('FRIED')
+        allow(plugin).to receive(:nagios_plugin_status).and_return(:chicken)
         expect(plugin.send(:nagios_plugin_output)).to match(/^FRIED CHICKEN$/)
       end
 
       it 'includes a custom plugin message if present' do
-        plugin.stub(:message => 'ALL U CAN EAT!')
+        allow(plugin).to receive(:message).and_return('ALL U CAN EAT!')
         expect(plugin.send(:nagios_plugin_output)).to match(/: ALL U CAN EAT!$/)
       end
 
-      it 'should not append message if message is empty' do
-        plugin.stub(:message => '')
+      it 'does not append message if message is empty' do
+        allow(plugin).to receive(:message).and_return('')
         expect(plugin.send(:nagios_plugin_output)).not_to match(/:/)
       end
 
